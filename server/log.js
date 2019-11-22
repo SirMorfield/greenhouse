33ee335077ce17ec6ee05ada6df01230a9017936
SirMorfield <joppe.koers@gmail.com>
2019-11-22 22:35:22 +0100

    pff

--- server/log.js
+++ server/log.js
@@ -4,13 +4,14 @@ const path = require('path')
 
 async function add() {
 	const bytes = await i2c.read()
-	const date = (new Date()).toLocaleString('en-GB', { hour12: false }).replace(/\,/g, '')
+	const date = (new Date()).toLocaleString('en-GB', { hour12: false })
 	let toWrite = [date, bytes.success]
 
 	if (bytes.success) {
 		toWrite.push(bytes.translated.temp)
 		toWrite.push(bytes.translated.hum)
 	}
+	toWrite = toWrite.map((value) => `"${value}"`)
 
 	toWrite = toWrite.join(',') + '\n'
 	await fs.appendFile(path.join(__dirname, 'log.csv'), toWrite);
