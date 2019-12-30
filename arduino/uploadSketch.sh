86419d1610d05213be851dda7dfd57ca9d2d919b
SirMorfield <joppe.koers@gmail.com>
2019-12-30 16:33:52 +0100

    fixed arduino upload

--- arduino/uploadSketch.sh
+++ arduino/uploadSketch.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-make -s -C arduino/sketch/ | egrep "Device|Program|Data|error"
+make -s -C sketch/
 
-# OPTION - ISP upload
-avrdude -p m328p -C arduino/sketch/avrdude.conf -c gpio -e -U flash:w:arduino/sketch/build-uno/sketch.hex
-avrdude -p m328p -c gpio -e -U flash:w:sketch/build-uno/sketch.hex
+sudo sh -c "echo 8 > /sys/class/gpio/unexport"
+sudo sh -c "echo 10 > /sys/class/gpio/unexport"
+sudo sh -c "echo 11 > /sys/class/gpio/unexport"
+sudo sh -c "echo 9 > /sys/class/gpio/unexport"
 
-# OPTION - usb upload
-# avrdude -C sketch/avrdude.conf -v -patmega328p -carduino -P /dev/ttyACM0 -b115200 -D -U flash:w:sketch/build-uno/sketch.hex
-
-# OPTION - normally executed by arduino ide
-# /home/joppe/Downloads/arduino-1.8.10-linux64/arduino-1.8.10/hardware/tools/avr/bin/avrdude -C/home/joppe/Downloads/arduino-1.8.10-linux64/arduino-1.8.10/hardware/tools/avr/etc/avrdude.conf -v -patmega328p -carduino -P/dev/ttyACM0 -b115200 -D -Uflash:w:/tmp/arduino_build_333508/Blink.ino.hex:i
+sudo avrdude -p m328p -C pi.conf -c linuxgpio -e -U flash:w:sketch/build-uno/sketch.hex
