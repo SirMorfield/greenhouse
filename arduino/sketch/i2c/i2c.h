161c91c90262fb424e5a8a758dba7ae4aa250b9c
SirMorfield <joppe.koers@gmail.com>
2020-01-14 13:15:37 +0100

    fixed formatting

--- arduino/sketch/i2c/i2c.h
+++ arduino/sketch/i2c/i2c.h
@@ -1,6 +1,5 @@
-class I2c
-{
-public:
-	uint8_t *getBytesToSend(uint16_t vars[], uint8_t numBytesToSend);
-	uint8_t generateChecksum(uint8_t bytes[], uint8_t size);
+class I2c {
+   public:
+    uint8_t *getBytesToSend(uint16_t vars[], uint8_t numBytesToSend);
+    uint8_t generateChecksum(uint8_t bytes[], uint8_t size);
 };
--- arduino/sketch/sketch.ino
+++ arduino/sketch/sketch.ino
@@ -28,39 +28,33 @@ DHT dht(8, DHT22);
 #define numVars 8
 uint16_t vars[numVars] = {};
 
-void respond()
-{
-	temp = dht.readTemperature() * 10;
-	hum = dht.readHumidity() * 10;
+void respond() {
+    temp = dht.readTemperature() * 10;
+    hum = dht.readHumidity() * 10;
 
-	digitalWrite(heaterPin, heaterOn == 0);
-	digitalWrite(dehumidifierPin, dehumidifierOn == 0);
+    digitalWrite(heaterPin, heaterOn == 0);
+    digitalWrite(dehumidifierPin, dehumidifierOn == 0);
 
-	analogWrite(lampPin, lampPWM);
-	analogWrite(inOutFanPin, inOutFanPWM);
-	analogWrite(pumpPin, pumpPWM);
+    analogWrite(lampPin, lampPWM);
+    analogWrite(inOutFanPin, inOutFanPWM);
+    analogWrite(pumpPin, pumpPWM);
 }
 
 uint16_t receiveDataPos = 0;
 uint8_t receiveBuffer[2] = {};
-void receiveData()
-{
-	uint8_t inByte = Wire.read();
-	if (receiveDataPos != 2)
-	{
-		receiveBuffer[receiveDataPos] = inByte;
-		receiveDataPos++;
-	}
-	else
-	{
-		uint8_t checkSum = i2c.generateChecksum(receiveBuffer, 2);
+void receiveData() {
+    uint8_t inByte = Wire.read();
+    if (receiveDataPos != 2) {
+        receiveBuffer[receiveDataPos] = inByte;
+        receiveDataPos++;
+    } else {
+        uint8_t checkSum = i2c.generateChecksum(receiveBuffer, 2);
 
-		if (checkSum == inByte)
-		{
-			vars[receiveBuffer[0]] = receiveBuffer[1];
-		}
-		receiveDataPos = 0;
-	}
+        if (checkSum == inByte) {
+            vars[receiveBuffer[0]] = receiveBuffer[1];
+        }
+        receiveDataPos = 0;
+    }
 }
 
 // Math.ceil((10 + 10 + 1 + 1 + 8 + 8 + 8 + 8 ) / 8) = 7
@@ -69,40 +63,36 @@ void receiveData()
 uint8_t numBytesToSend = 8;
 uint8_t *bytesToSend;
 uint8_t sendDataPos = 0;
-void sendData()
-{
-	if (sendDataPos == 0)
-	{
-		respond();
-		bytesToSend = i2c.getBytesToSend(vars, numBytesToSend);
-	}
-	Wire.write(bytesToSend[sendDataPos]);
-	if (sendDataPos++ == (numBytesToSend - 1))
-	{
-		sendDataPos = 0;
-	}
+void sendData() {
+    if (sendDataPos == 0) {
+        respond();
+        bytesToSend = i2c.getBytesToSend(vars, numBytesToSend);
+    }
+    Wire.write(bytesToSend[sendDataPos]);
+    if (sendDataPos++ == (numBytesToSend - 1)) {
+        sendDataPos = 0;
+    }
 }
 
-void setup()
-{
-	pinMode(greenLedPin, OUTPUT);
-	digitalWrite(greenLedPin, HIGH);
+void setup() {
+    pinMode(greenLedPin, OUTPUT);
+    digitalWrite(greenLedPin, HIGH);
 
-	pinMode(heaterPin, OUTPUT);
-	pinMode(dehumidifierPin, OUTPUT);
-	pinMode(lampPin, OUTPUT);
-	pinMode(inOutFanPin, OUTPUT);
-	pinMode(pumpPin, OUTPUT);
-	pinMode(sensorFanPin, OUTPUT);
+    pinMode(heaterPin, OUTPUT);
+    pinMode(dehumidifierPin, OUTPUT);
+    pinMode(lampPin, OUTPUT);
+    pinMode(inOutFanPin, OUTPUT);
+    pinMode(pumpPin, OUTPUT);
+    pinMode(sensorFanPin, OUTPUT);
 
-	dht.begin();
+    dht.begin();
 
-	Wire.begin(0x08);
-	Wire.onReceive(receiveData);
-	Wire.onRequest(sendData);
+    Wire.begin(0x08);
+    Wire.onReceive(receiveData);
+    Wire.onRequest(sendData);
 
-	delay(1000);
-	digitalWrite(greenLedPin, LOW);
+    delay(1000);
+    digitalWrite(greenLedPin, LOW);
 }
 
 void loop() {}
